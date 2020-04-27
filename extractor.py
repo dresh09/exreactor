946b3623f8450becb32a6bc068db29c71b4b33db
dresh09 <stillon2008@gmail.com>
2020-04-27 20:08:24 +0100

    Add files via upload

---
+++ extractor.py
@@ -0,0 +1,17 @@
+import re
+
+#The welcome message
+print('''Welcome to @Dresh09 email extractor,
+please paste your clipboard in iput.txt to extractor emails''')
+
+def emailReg():
+    fi = open('input.txt', 'r', encoding='utf_8')
+    fo = open('result.txt', 'a')
+
+    for line in fi :
+        pattern = re.compile('[a-zA-Z0-9\-\.]+@[a-zA-Z0-9\-\.]+\.[a-zA-Z0-9]+')
+        result = pattern.findall(line)
+        for i in result:
+            print(i, file = fo)
+    print('Done')
+emailReg()
